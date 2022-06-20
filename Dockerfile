7d947624626bd0df4de04e714f9ebc3c2e6d85f3
Kamil Porwit <kamilporwit93@gmail.com>
2022-06-20 14:43:19 +0200

    Set up debug to false

--- Dockerfile
+++ Dockerfile
@@ -22,4 +22,4 @@ COPY . $DockerHOME
 RUN pip install -r requirements.txt  
 EXPOSE 8000
 # start server  
-CMD python manage.py runserver
+CMD python manage.py runserver 0.0.0.0:8000
--- VinylLibApp/settings.py
+++ VinylLibApp/settings.py
@@ -23,11 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pk3333q!(rooncr)!9xf5$ebh#_&3wot4%h_g0=00gzbidk)$('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [
-    '*',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
